3dd7e555f1958ab317f7fcdfe28ec668c3293632
ArRolin <gmarrolin@gmail.com>
2022-08-29 16:41:58 +0600

    05 Exercise Printing Variables Section Added

---
+++ 05_exercise_printing_variables/main.dart
@@ -0,0 +1,11 @@
+void main() {
+  String firstName = "Ashiqur";
+  String lastName = "Rahaman";
+  int age = 30;
+  double height = 1.74;
+
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(height);
+}
